5b10b0a0ccb5bb9de53830499cd5df04d55a057a
Jimmy Dong <jndong88@gmail.com>
2017-02-02 17:46:37 -0800

    initial frame work and method overide

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -17,10 +19,14 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+require('./config/database');
+
 
 app.use('/', index);
 app.use('/users', users);

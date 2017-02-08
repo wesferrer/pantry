5f43563000e29733d3aa1ce973249ede8cab6c06
Elena <elena.korotich@gmail.com>
2017-02-08 12:31:26 -0800

    fixed recipes routes

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,16 +2,14 @@ var express = require('express');
 var router = express.Router();
 var recipesCtrl = require('../controllers/recipes');
 
-
-router.get('/:recipeId', isLoggedIn, recipesCtrl.addFav);
+router.get('/:recipeId/fav', isLoggedIn, recipesCtrl.addFav);
+router.get('/:recipeId', recipesCtrl.show);
+router.get('/', isLoggedIn, recipesCtrl.index);
 
 function isLoggedIn(req, res, next) {
   if ( req.isAuthenticated() ) return next();
   res.redirect('/auth/google');
 }
-router.get('/:recipeId/fav', recipesCtrl.addFav);
-router.get('/:recipeId', recipesCtrl.show);
-router.get('/', recipesCtrl.index);
 
 module.exports = router;
 

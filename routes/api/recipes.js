d7b1a720bb9cb6b4de9aec55339590b62fafd1d0
Wes Ferrer <wesferrer@gmail.copm>
2017-02-08 14:14:42 -0800

    added api production

--- routes/api/recipes.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.router();
-var recipesCtrl = require('../controllers/api/recipes');
-
-route.get('/', recipesCtrl.getAllRecipes);
-route.get('/:id', recipesCtrl.getOneRecipe);

19f18a259f3013081088a98fa48e40dbe625b97c
Elena <elena.korotich@gmail.com>
2017-02-10 09:55:12 -0800

    final changes

--- models/user.js
+++ models/user.js
@@ -2,18 +2,12 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var shoppingSchema = new Schema ({
-	recipe: [{type: Schema.Types.ObjectId, ref: 'Recipe'}],
-	grocery: [{type: Schema.Types.ObjectId, ref: 'Ingredient'}]
-});
-
 var userSchema = new Schema({
 	name: String,
 	email: String,
 	favorites: [{type: Schema.Types.ObjectId, ref: 'Recipe'}],
 	pantry: [String],
 	restrictions: [String],
-	shoppingList: [shoppingSchema],
   googleId: String
 });
 
